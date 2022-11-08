76ef60f9cf834c16aa5007e021cc861d44b7726a
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-11-08 17:20:33 +0100

    Initial setup for angular app

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'my-app';
+  btnType: string = 'submit';
+  btnColor: string = 'red';
+}
