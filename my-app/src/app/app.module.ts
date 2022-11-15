c630057ceeca64e4c644711f8f34799ef41775a8
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-11-15 16:00:23 +0100

    Imported the forms module to use ngModel

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -8,15 +8,23 @@ import { IconComponent } from './icon/icon.component';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TextInputComponent } from './text-input/text-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, IconComponent],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    IconComponent,
+    TextInputComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
