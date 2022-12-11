6d9bbe58ee956b3a4368545a234bb04c92d11bcc
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-11 17:03:13 +0100

    Added reusable label for textInput

---
+++ my-app/src/app/components/text-input/text-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+export type TextInputComponentProps = {
+  textWidth: string;
+  textHeight: string;
+  iconLabel: string;
+};
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css'],
+})
+export class TextInputComponent implements OnInit {
+  @Output() textInputEmitter = new EventEmitter<string>();
+  @Input() textWidth: string = '';
+  @Input() textHeight: string = '';
+  @Input() iconLabel: string = '';
+  @Input() fieldLabel: string = '';
+  enteredText: string = '';
+
+  textInputHandler(event: Event) {
+    this.textInputEmitter.emit((<HTMLInputElement>event.target).value);
+  }
+  clearTextInputHandler() {
+    this.enteredText = '';
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
