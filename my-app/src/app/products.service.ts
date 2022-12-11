77b37124d03a22fca4b69d687ee8204f3b3be2a7
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-11 16:03:46 +0100

    Created product service

---
+++ my-app/src/app/products.service.ts
@@ -0,0 +1,19 @@
+export class ProductsService {
+  Products = [
+    {
+      name: 'Banana',
+      quantity: 10,
+      price: 30,
+    },
+    {
+      name: 'Apple',
+      quantity: 15,
+      price: 20,
+    },
+    {
+      name: 'Pinneapple',
+      quantity: 20,
+      price: 40,
+    },
+  ];
+}
