496d7db0af78c74752e9ab11060ebfe5185f20a3
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-23 12:14:45 +0100

    Utilised login service and displayed api response

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,7 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LoginService } from '../login.service';
 
+//https://stackoverflow.com/questions/14627399/setting-authorization-header-of-httpclient
+//Create a login form
+//create a login service with basic authentication(with custom user details service and also py passing directly
+//username and password in security conffiguration file)
+//utilise the service
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,13 +16,17 @@ import { LoginService } from '../login.service';
 })
 export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
-    email: new FormControl(''),
+    username: new FormControl(''),
     password: new FormControl(''),
   });
+
   constructor(private loginService: LoginService) {}
 
   submit() {
-    this.loginService.onLoginSubmit(this.loginForm.value);
+    this.loginService.onLoginSubmit(this.loginForm.value).subscribe({
+      next: (data) => console.log('Data is', data),
+      error: (error) => console.log('Error is ', error),
+    });
   }
 
   ngOnInit(): void {}
