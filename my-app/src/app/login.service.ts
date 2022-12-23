fad961f6c5e010be5577f9821e6020a93e046301
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-23 12:15:44 +0100

    Implemented login service with auth header

--- my-app/src/app/login.service.ts
+++ my-app/src/app/login.service.ts
@@ -1,5 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { catchError } from 'rxjs';
+
+//https://www.javainuse.com/spring/ang7-basic-interceptor
+@Injectable({ providedIn: 'root' })
 export class LoginService {
+  baseURL: string = 'http://localhost:8080/';
+
+  constructor(private http: HttpClient) {}
+
   onLoginSubmit(data: any) {
-    console.log('Login form submitted', data);
+    var encodedAuth = window.btoa(data.username + ':' + data.password);
+
+    console.log('Encoded value is', encodedAuth);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + encodedAuth,
+      }),
+    };
+
+    const body = JSON.stringify(data);
+    console.log(httpOptions.headers);
+    return this.http.post(this.baseURL + 'User/Login', body, httpOptions);
   }
 }
