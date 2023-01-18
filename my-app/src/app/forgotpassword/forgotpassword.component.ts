5b2648f45241088860fd208b93a668f585432856
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-18 08:59:54 +0100

    Utilised the service for forgot password

--- my-app/src/app/forgotpassword/forgotpassword.component.ts
+++ my-app/src/app/forgotpassword/forgotpassword.component.ts
@@ -8,12 +8,15 @@ import { forgotpasswordService } from '../forgot-password.service';
 })
 export class ForgotpasswordComponent implements OnInit {
   email: String = '';
+  emailGenerated: boolean = false;
   constructor(private service: forgotpasswordService) {}
 
   getToken() {
-    this.service
-      .onForgotPassword(this.email)
-      .subscribe({ next: (data: object) => console.log(data) });
+    this.service.onForgotPassword(this.email).subscribe({
+      next: (data: Object) => {
+        console.log('Data is ' + JSON.parse(JSON.stringify(data)));
+      },
+    });
   }
 
   ngOnInit(): void {}
